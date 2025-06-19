67f296a272d1e8cff92e533003ab3dc4528cefa4
gustavoscardino <gustavoscardino@hotmail.com>
2025-06-19 12:06:20 +0200

    code

---
+++ leetcode/BinaryTree/countNodes/countNodes.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>  
+#include <queue>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */ 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left && right) return root; 
+        return left ? left : right;     
+    }
+};
+
+
+int main() {
+    return 0;
+}
