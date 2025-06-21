ea49368c885026dfb0b6c9b86799a152124a5ec2
gustavoscardino <gustavoscardino@hotmail.com>
2025-06-21 21:27:40 +0200

    code

--- leetcode/BinaryTree/levelOrder/levelOrder.cpp
+++ leetcode/BinaryTree/levelOrder/levelOrder.cpp
@@ -27,42 +27,30 @@ struct TreeNode {
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class BSTIterator {
-private:
-    stack<TreeNode*> st;
-
-    void pushLeft(TreeNode* node) {
-        while (node) {
-            st.push(node);
-            node = node->left;
-        }
-    }
-
+class Solution {
 public:
-    BSTIterator(TreeNode* root) {
-        pushLeft(root);
-    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root) return res;
 
-    int next() {
-        TreeNode* node = st.top(); st.pop();
-        if (node->right) {
-            pushLeft(node->right);
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int level = q.size();
+            vector<int> thisLevel;
+
+            for(int i=0; i<level; i++){
+                TreeNode* current = q.front(); q.pop();
+                thisLevel.push_back(current->val);
+                if(current->left) q.push(current->left);
+                if(current->right) q.push(current->right);
+            }
+            res.push_back(thisLevel);
         }
-        return node->val;
-    }
-
-    bool hasNext() {
-        return !st.empty();
+        return res;
     }
 };
-
-
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator* obj = new BSTIterator(root);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
 
 int main() {
     return 0;
